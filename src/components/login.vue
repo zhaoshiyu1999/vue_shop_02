28f30e843f68fe23e58df5fb39f0d787b0af9017
zhaoshiyu <771350815@qq.com>
2020-04-03 14:04:12 +0800

    完成用户列表功能的开发

--- src/components/login.vue
+++ src/components/login.vue
@@ -3,7 +3,7 @@
     <div class="login_box">
       <!-- 头像区域 -->
       <div class="avatar_box">
-        <img src="../assets/logo.png" alt />
+        <img src="../assets/头像.jpg" alt />
       </div>
       <!-- 登录表单区域 -->
       <el-form ref="loginFormRef" class="login_form" :model="loginForm" :rules="loginFormRules">
--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,28 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/login.vue'
 import Home from '../components/home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home },
+  { 
+    path: '/home', 
+    component: Home, 
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users },
+    ] 
+  },
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  base: '/vue-admin'
 })
 
 // 挂载路由导航守卫
